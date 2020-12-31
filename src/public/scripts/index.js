f30d3cc110e5fa224b344a8bf2d94b2c1636795e
Brooks Becton <bbecton@babelstreet.com>
2020-12-31 15:16:02 -0500

    Style Homepage

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -5,6 +5,7 @@ const App = () => {
   const [faces, setFaces] = React.useState([]);
   const [selectedHead, setSelectedHead] = React.useState("");
   const [selectedFace, setSelectedFace] = React.useState("");
+
   const getAvailableOptions = async () => {
     const availableOptions = await fetch("/api/images/info").then((r) =>
       r.json()
@@ -31,6 +32,42 @@ const App = () => {
     }
   }, [faces]);
 
+  function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      var successful = document.execCommand("copy");
+      var msg = successful ? "successful" : "unsuccessful";
+      console.log("Fallback: Copying text command was " + msg);
+    } catch (err) {
+      console.error("Fallback: Oops, unable to copy", err);
+    }
+
+    document.body.removeChild(textArea);
+  }
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(
+      function () {},
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  }
+
   const handleFaceSelect = React.useCallback(
     (e) => {
       setSelectedFace(e.target.value);
@@ -44,60 +81,117 @@ const App = () => {
     [setSelectedHead]
   );
 
+  const handleCopy = React.useCallback(() => {
+    copyTextToClipboard(
+      `http://localhost:3000/api/images/?face=${selectedFace}&head=${selectedHead}`
+    );
+  }, [selectedFace, selectedHead]);
+
   return (
-    <div className="container">
-      <div className="app-container">
-        <h1 className="slant-background">Open Peeps</h1>
-        <div className="main-container">
-          <div className="picker">
-            <h3 className="slant-background">Faces</h3>
-            {faces.map((f) => {
-              return (
-                <label key={f}>
-                  <input
-                    checked={f === selectedFace}
-                    label={f}
-                    type="radio"
-                    name="face"
-                    value={f}
-                    onChange={handleFaceSelect}
-                  />
-                  {f}
-                </label>
-              );
-            })}
-            <h3 className="slant-background">Heads</h3>
-            {heads.map((h) => {
-              return (
-                <label key={h}>
-                  <input
-                    checked={h === selectedHead}
-                    label={h}
-                    type="radio"
-                    name="head"
-                    value={h}
-                    onChange={handleHeadSelect}
-                  />
-                  {h}
-                </label>
-              );
-            })}
-          </div>
-          <div className="preview">
-            {selectedFace !== "" && selectedHead !== "" && (
-              <img
-                src={`/api/images?head=${selectedHead}&face=${selectedFace}`}
-                alt={`preview of ${selectedHead} head and ${selectedFace} face`}
-              />
-            )}
-          </div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        width: "70%",
+        margin: "auto",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Open Peeps Avatar API</h1>
+        <p>
+          Open Peeps is A hand-drawn illustration library. This API provides a
+          way to dynamically request different avatar images.
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            width: 200,
+            height: 200,
+            backgroundColor: "red",
+            borderRadius: 25,
+            padding: 10,
+          }}
+        >
+          {selectedFace && selectedHead && (
+            <img
+              style={{ width: "100%" }}
+              src={`http://localhost:3000/api/images/?face=${selectedFace}&head=${selectedHead}`}
+              alt={`Open Peeps ${selectedHead} head with ${selectedFace} face`}
+            />
+          )}
+        </div>
+        <div>
+          <pre
+            style={{ display: "inline" }}
+          >{`http://localhost:3000/api/images/?face=`}</pre>
+          <mark>{selectedFace}</mark>
+          <pre style={{ display: "inline" }}>{`&head=`}</pre>
+          <mark>{selectedHead}</mark>
+          <button onClick={handleCopy}>Copy</button>
         </div>
       </div>
-      <div className="shoutout">
-        <p className="text-md">Open Peeps</p>
-        <a target="_blank" href="https://www.openpeeps.com/">
-          <button>Visit Open Peeps</button>
-        </a>
+
+      <h2>Options</h2>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div>
+          <h3>Faces</h3>
+          <ul style={{ display: "flex", flexDirection: "column" }}>
+            {faces.map((f) => (
+              <label key={f}>
+                <input
+                  checked={f === selectedFace}
+                  label={f}
+                  type="radio"
+                  name="face"
+                  value={f}
+                  onChange={handleFaceSelect}
+                />
+                {f}
+              </label>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>Heads</h3>
+          <ul style={{ display: "flex", flexDirection: "column" }}>
+            {heads.map((f) => (
+              <label key={f}>
+                <input
+                  checked={f === selectedHead}
+                  label={f}
+                  type="radio"
+                  name="head"
+                  value={f}
+                  onChange={handleHeadSelect}
+                />
+                {f}
+              </label>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
